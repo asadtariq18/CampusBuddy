5c1e379b95cf90a72e2ec16da7b4ebe128c7bb74
asadtariq18 <asadtariq070@gmail>
2021-10-03 17:21:54 +0500

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./router/DrawerNavigator/drawerNavigator";
