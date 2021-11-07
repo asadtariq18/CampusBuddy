a5e3101bcb7eb7b0a499b4af9e29c6cfc42a8af9
asadtariq18 <asadtariq070@gmail.com>
2021-11-08 02:41:52 +0500

    Integrated with firebase

--- src/screens/SearchScreen/index.js
+++ src/screens/SearchScreen/index.js
@@ -1,32 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { SafeAreaView, StatusBar, Text } from "react-native";
-import {
-  Header,
-  Item,
-  Input,
-  Icon,
-  Button,
-} from "native-base";
+import { Header, Item, Input, Icon, Button } from "native-base";
 import { COLORS } from "../../Constants/COLORS";
 import styles from "../SearchScreen/style";
+import SearchList from "../../components/SearchList";
 
-const SearchScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <Header
-      searchBar
-      rounded
-      style={{ backgroundColor: COLORS.background_dark }}
-    >
-      <Item style={{ backgroundColor: COLORS.secondary }}>
-        <Icon name="ios-search" />
-        <Input placeholder="Search" />
-      </Item>
-      <StatusBar backgroundColor={COLORS.background_dark} />
-      <Button transparent>
-        <Text>Search</Text>
-      </Button>
-    </Header>
-  </SafeAreaView>
-);
+const SearchScreen = () => {
+  const [query, setQuery] = useState('');
+
+  const onChange = (input) => {
+    setQuery(input);
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header
+        searchBar
+        rounded
+        style={{ backgroundColor: COLORS.background_dark }}
+      >
+        <Item style={{ backgroundColor: COLORS.secondary }}>
+          <Icon name="ios-search" />
+          <Input
+            placeholder="Search"
+            onChangeText={(input) => onChange(input)}
+            style={{ color: COLORS.font }}
+          />
+        </Item>
+        <StatusBar backgroundColor={COLORS.background_dark} />
+        <Button transparent>
+          <Text>Search</Text>
+        </Button>
+      </Header>
+      <SearchList query={query} />
+    </SafeAreaView>
+  );
+};
 
 export default SearchScreen;
