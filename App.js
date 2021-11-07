df72177df891491c2abe985784770d7e22f1cca1
asadtariq18 <asadtariq070@gmail.com>
2021-11-07 23:59:34 +0500

    Added some minor fixes

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { registerRootComponent } from "expo";
 import React, { useEffect, useState } from "react";
+import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoadingScreen from "./src/screens/LoadingScreen";
@@ -16,7 +17,7 @@ const Stack = createStackNavigator();
 export default function App() {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
-
+  LogBox.ignoreLogs(["Setting a timer for a long period of time"]);
   function onAuthStateChanged(user) {
     setUser(user);
     if (initializing) setInitializing(false);
