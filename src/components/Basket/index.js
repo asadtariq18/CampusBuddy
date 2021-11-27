edfb25604bb5c9d1fad5acb168d475b863cc6626
asadtariq18 <asadtariq070@gmail.com>
2021-11-28 01:47:13 +0500

    Design improvement and Redux function improvement

--- src/components/Basket/index.js
+++ src/components/Basket/index.js
@@ -12,18 +12,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { setBasket } from "../../Redux/OrderFood/actions";
 
 const Basket = () => {
-    const dispatch = useDispatch()
-    const basket = useSelector((state) => state.orderFood.basket);
-  const [refreshing, setRefreshing] = React.useState(false);
-  const onRefresh = React.useCallback(async () => {
-    setRefreshing(true);
-    try {
-      setRefreshing(false);
-      ToastAndroid.show("Updated", ToastAndroid.SHORT);
-    } catch (error) {
-      console.error(error);
-    }
-  }, [refreshing]);
+  const dispatch = useDispatch();
+  const basket = useSelector((state) => state.orderFood.basket);
   return (
     <FlatList
       contentContainerStyle={{
@@ -34,16 +24,13 @@ const Basket = () => {
       data={basket}
       keyExtractor={({ id }) => id}
       renderItem={({ item }) => (
-        <BasketItem image={item.image} name={item.name} price={item.price} />
-      )}
-      refreshControl={
-        <RefreshControl
-          progressBackgroundColor={COLORS.background_dark}
-          colors={[COLORS.primary]}
-          refreshing={refreshing}
-          onRefresh={onRefresh}
+        <BasketItem
+          image={item.image}
+          name={item.name}
+          price={item.price}
+          quantity={item.quantity}
         />
-      }
+      )}
     />
   );
 };
