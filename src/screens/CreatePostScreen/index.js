b3570e471b0a18e1a7618cc824bab80851e5f08e
asadtariq18 <asadtariq070@gmail.com>
2021-12-10 03:32:55 +0500

    Added Text-Only posts

--- src/screens/CreatePostScreen/index.js
+++ src/screens/CreatePostScreen/index.js
@@ -19,17 +19,23 @@ import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 import Database from "../../Database/database";
 import { useDispatch, useSelector } from "react-redux";
-import { setCaption, setType, setPrivacy, setImage } from "../../Redux/CreatePost/actions";
+import {
+  setCaption,
+  setType,
+  setPrivacy,
+  setImage,
+} from "../../Redux/CreatePost/actions";
 import { setPosts } from "../../Redux/Home/actions";
 import database from "../../Database/database";
 const CreatePostScreen = () => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
-  const icon = "https://icon-library.com/images/add-photo-icon/add-photo-icon-19.jpg"
-  const caption = useSelector((state)=> state.createPost.caption);
-  const type = useSelector((state)=> state.createPost.type);
-  const privacy = useSelector((state)=> state.createPost.privacy);
-  const image = useSelector((state)=> state.createPost.image);
+  const icon =
+    "https://icon-library.com/images/add-photo-icon/add-photo-icon-19.jpg";
+  const caption = useSelector((state) => state.createPost.caption);
+  const type = useSelector((state) => state.createPost.type);
+  const privacy = useSelector((state) => state.createPost.privacy);
+  const image = useSelector((state) => state.createPost.image);
 
   const onChangeCaption = (input) => {
     dispatch(setCaption(input));
@@ -59,7 +65,7 @@ const CreatePostScreen = () => {
         aspect: [1, 1],
         quality: 0.5,
       });
-        dispatch(setImage(data.uri));
+      dispatch(setImage(data.uri));
     } else {
       Alert.alert("you need to give permission to work");
     }
@@ -68,15 +74,24 @@ const CreatePostScreen = () => {
   const postPressed = () => {
     try {
       if (image === icon) {
-        ToastAndroid.show("No image selected", ToastAndroid.SHORT);
+        if (caption !== "") {
+          ToastAndroid.show("Uploading your post", ToastAndroid.LONG);
+          Database.uploadUserPost(caption, privacy, type, null);
+          dispatch(setPosts(database.getPosts()));
+          navigation.navigate("Home");
+
+          dispatch(setCaption(""));
+          dispatch(setImage(icon));
+          dispatch(setType("status"));
+          dispatch(setPrivacy("public"));
+          ToastAndroid.show("Post Uploaded", ToastAndroid.SHORT);
+        } else {
+          ToastAndroid.show("Empty Post", ToastAndroid.LONG);
+        }
       } else {
         ToastAndroid.show("Uploading your post", ToastAndroid.LONG);
         Database.uploadUserPost(caption, privacy, type, image);
-            dispatch(
-              setPosts(
-               database.getPosts()
-              )
-            );
+        dispatch(setPosts(database.getPosts()));
         navigation.navigate("Home");
 
         dispatch(setCaption(""));
@@ -86,7 +101,7 @@ const CreatePostScreen = () => {
         ToastAndroid.show("Post Uploaded", ToastAndroid.SHORT);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
       ToastAndroid.show(error, ToastAndroid.SHORT);
     }
   };
@@ -138,7 +153,9 @@ const CreatePostScreen = () => {
       </ScrollView>
       <View style={styles.cardView}>
         <View style={{ flexDirection: "row", marginTop: 8 }}>
-          <TouchableWithoutFeedback onPress={() => dispatch(setPrivacy("public"))}>
+          <TouchableWithoutFeedback
+            onPress={() => dispatch(setPrivacy("public"))}
+          >
             {privacy === "public" ? (
               <View style={styles.buttonView}>
                 <Text style={styles.button_pressed}>Public</Text>
@@ -149,7 +166,9 @@ const CreatePostScreen = () => {
               </View>
             )}
           </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback onPress={() => dispatch(setPrivacy("private"))}>
+          <TouchableWithoutFeedback
+            onPress={() => dispatch(setPrivacy("private"))}
+          >
             {privacy === "private" ? (
               <View style={styles.buttonView}>
                 <Text style={styles.button_pressed}>Private</Text>
@@ -160,52 +179,52 @@ const CreatePostScreen = () => {
               </View>
             )}
           </TouchableWithoutFeedback>
-          </View>
-          <View style={{ flexDirection: "row", marginTop: 8 }}>
-            <TouchableWithoutFeedback onPress={() => dispatch(setType("status"))}>
-              {type === "status" ? (
-                <View style={styles.buttonView}>
-                  <Text style={styles.button_pressed}>Status</Text>
-                </View>
-              ) : (
-                <View style={styles.buttonView}>
-                  <Text style={styles.button}>Status</Text>
-                </View>
-              )}
-            </TouchableWithoutFeedback>
-            <TouchableWithoutFeedback onPress={() => dispatch(setType("ask"))}>
-              {type === "ask" ? (
-                <View style={styles.buttonView}>
-                  <Text style={styles.button_pressed}>Ask</Text>
-                </View>
-              ) : (
-                <View style={styles.buttonView}>
-                  <Text style={styles.button}>Ask</Text>
-                </View>
-              )}
-            </TouchableWithoutFeedback>
-            <TouchableWithoutFeedback onPress={() => dispatch(setType("lost"))}>
-              {type === "lost" ? (
-                <View style={styles.buttonView}>
-                  <Text style={styles.button_pressed}>Lost</Text>
-                </View>
-              ) : (
-                <View style={styles.buttonView}>
-                  <Text style={styles.button}>Lost</Text>
-                </View>
-              )}
-            </TouchableWithoutFeedback>
-            <TouchableWithoutFeedback onPress={() => dispatch(setType("found"))}>
-              {type === "found" ? (
-                <View style={styles.buttonView}>
-                  <Text style={styles.button_pressed}>Found</Text>
-                </View>
-              ) : (
-                <View style={styles.buttonView}>
-                  <Text style={styles.button}>Found</Text>
-                </View>
-              )}
-            </TouchableWithoutFeedback>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 8 }}>
+          <TouchableWithoutFeedback onPress={() => dispatch(setType("status"))}>
+            {type === "status" ? (
+              <View style={styles.buttonView}>
+                <Text style={styles.button_pressed}>Status</Text>
+              </View>
+            ) : (
+              <View style={styles.buttonView}>
+                <Text style={styles.button}>Status</Text>
+              </View>
+            )}
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => dispatch(setType("ask"))}>
+            {type === "ask" ? (
+              <View style={styles.buttonView}>
+                <Text style={styles.button_pressed}>Ask</Text>
+              </View>
+            ) : (
+              <View style={styles.buttonView}>
+                <Text style={styles.button}>Ask</Text>
+              </View>
+            )}
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => dispatch(setType("lost"))}>
+            {type === "lost" ? (
+              <View style={styles.buttonView}>
+                <Text style={styles.button_pressed}>Lost</Text>
+              </View>
+            ) : (
+              <View style={styles.buttonView}>
+                <Text style={styles.button}>Lost</Text>
+              </View>
+            )}
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => dispatch(setType("found"))}>
+            {type === "found" ? (
+              <View style={styles.buttonView}>
+                <Text style={styles.button_pressed}>Found</Text>
+              </View>
+            ) : (
+              <View style={styles.buttonView}>
+                <Text style={styles.button}>Found</Text>
+              </View>
+            )}
+          </TouchableWithoutFeedback>
         </View>
       </View>
       <View></View>
